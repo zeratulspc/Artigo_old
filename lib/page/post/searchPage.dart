204b5beaddaf8d6572c604d860929818468aa7d4
zeratulspc <zeratulspc@gmail.com>
2020-09-08 21:34:18 +0900

    chore:0.1.6_200908_2
    Post 검색기능
    
    todo User 검색용 Card 만들기

--- lib/page/post/searchPage.dart
+++ lib/page/post/searchPage.dart
@@ -2,12 +2,23 @@ import 'dart:collection';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-import 'package:Artigo/fnc/postDB.dart';
 import 'package:Artigo/fnc/user.dart';
+import 'package:Artigo/fnc/postDB.dart';
+import 'package:Artigo/fnc/emotion.dart';
+import 'package:Artigo/page/basicDialogs.dart';
+import 'package:Artigo/page/emotion/emotionInput.dart';
 import 'package:Artigo/page/post/postCard.dart';
+import 'package:Artigo/page/post/editPost.dart';
+import 'package:Artigo/page/comment/commentList.dart';
+import 'package:Artigo/page/emotion/emotionList.dart';
 
 class SearchPage extends StatefulWidget {
+  final VoidCallback navigateToMyProfile;
+
+  SearchPage({this.navigateToMyProfile});
+
   @override
   _SearchPageState createState() => _SearchPageState();
 }
@@ -15,19 +26,27 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   PostDBFNC postDBFNC = PostDBFNC();
   UserDBFNC userDBFNC = UserDBFNC();
+  BasicDialogs basicDialogs = BasicDialogs();
 
   List<Post> posts = List();
   List<UserAdditionalInfo> users = List();
   List<Widget> frontWidgets = List();
 
   User currentUser;
-
+  UserAdditionalInfo user;
   TextEditingController textController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     currentUser = userDBFNC.getUser();
+    userDBFNC.getUserInfo(currentUser.uid).then((userInfo){
+      if(this.mounted) {
+        setState(() {
+          user = userInfo;
+        });
+      }
+    });
     postDBFNC.postDBRef.once().then((snapshot) {
       LinkedHashMap<dynamic, dynamic>linkedHashMap = snapshot.value;
       linkedHashMap.forEach((key, value) async {
@@ -61,7 +80,6 @@ class _SearchPageState extends State<SearchPage> {
           if(e.userName == keyword) {
             // 이 오브젝트를 프론트 위젯에 추가
             // 형태 : TODO 새로 만들기....
-            print(e.userName);
           }
         });
         // 게시글 검색
@@ -69,19 +87,106 @@ class _SearchPageState extends State<SearchPage> {
           if(e.body.contains(keyword)) {
             frontWidgets.add(
                 PostCard(
+                  moreOption: (){
+                    if(currentUser.uid == e.uploaderUID || user.role == "ADMIN")
+                      postModalBottomSheet(context, e);
+                  },
+                  dislikeToPost: (){
+                    DatabaseReference likeDBRef = FirebaseDatabase.instance.reference().child("Posts").child(e.key);
+                    EmotionDBFNC(emotionDBRef: likeDBRef).dislike(currentUser.uid);
+                  },
+                  likeToPost: () {
+                    DatabaseReference likeDBRef = FirebaseDatabase.instance.reference().child("Posts").child(e.key);
+                    EmotionInput(likeDBRef, currentUser.uid, e.uploaderUID, (){}).showEmotionPicker(context);
+                  },
+                  showCommentSheet: () {
+                    showModalBottomSheet(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(15))),
+                      isScrollControlled: true,
+                      context: context,
+                      builder: (context) {
+                        return CommentList(
+                          getPost: () async {
+                            Post data = await postDBFNC.getPost(e.key);
+                            setState(() {
+                              e = data;
+                            });
+                          },
+                          navigateToMyProfile: widget.navigateToMyProfile,
+                          postKey: e.key,
+                          currentUser: currentUser,
+                        );
+                      },
+                    );
+                  },
+                  showLikeSheet: () {
+                    showModalBottomSheet(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(15))),
+                      isScrollControlled: true,
+                      context: context,
+                      builder: (context) {
+                        return EmotionList(
+                          postKey: e.key,
+                          currentUser: currentUser,
+                          navigateToMyProfile: widget.navigateToMyProfile,
+                        );
+                      },
+                    );
+                  },
                   item: e,
                   uploader: e.uploader,
                   screenSize: screenSize,
                   currentUser: currentUser
                 ).postCard(context),
             );
-          print(e.body);
           }
         });
       });
     } else {
       // 다시 시도해주세요! OR 게시글이 없습니다
     }
+  }
+
+  void postModalBottomSheet(context, Post post) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext _context){
+          return Container(
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                    leading: Icon(Icons.edit),
+                    title: Text('게시글 수정'),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>
+                          EditPost(postCase: 2, initialPost: post, uploader: user, currentUser: currentUser,)));
+                    }
+                ),
+                ListTile(
+                  leading: Icon(Icons.delete),
+                  title: Text("게시글삭제"),
+                  subtitle: Text("다시 되돌릴 수 없습니다!"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    basicDialogs.dialogWithFunction(
+                        context, "게시글 삭제", "게시글을 삭제하시겠습니까?", () {
+                      Navigator.pop(context);
+                      basicDialogs.showLoading(context, "게시글을 삭제하는 중입니다.");
+                      if(post.attach != null) {
+                        postDBFNC.deletePostStorage(post.key, post.attach)
+                            .then((_)=>postDBFNC.deletePost(post.key).then((_)=>Navigator.pop(context)));
+                      } else {
+                        postDBFNC.deletePost(post.key).then((_)=>Navigator.pop(context));
+                      }
+                    });
+                  },
+                ),
+              ],
+            ),
+          );
+        }
+    );
   }
 
 
